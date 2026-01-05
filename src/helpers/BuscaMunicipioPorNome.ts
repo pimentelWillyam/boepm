8d84308052a3e6cbf4a8bbb6ba60ba44eaee9236
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-01-05 14:03:11 -0300

    feat: adicionando busca municipio por nome

--- src/helpers/BuscaMunicipioPorNome.ts
+++ src/helpers/BuscaMunicipioPorNome.ts
@@ -6,12 +6,12 @@ type Municipio = {
     "AIS": string
 }
 
-import municipiosCompletos from '../pages/Envolvidos/Endereco/municipiosCompletos.json'
+import municipiosCompletos from '../pages/Envolvidos/Endereco/municipiosCompletosFinal.json'
 
 export class BuscaMunicipioPorNome {
     private static listaMunicipios: Municipio[] = municipiosCompletos as Municipio[]
     public static execute(nomeMunicipio: string | undefined): Municipio | null {
-        if (!nomeMunicipio) return null;
+        console.log("nome", nomeMunicipio)
         const municipioEncontrado = this.listaMunicipios.find(municipio => municipio.MUNICIPIO === nomeMunicipio);
         return municipioEncontrado ? municipioEncontrado : null;
     }
