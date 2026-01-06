affad02b25654df59d9ad25d05345f1204f7deeb
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-01-06 14:52:19 -0300

    feat: adicionando buscador de batalhao reduzido por batalhao extendido

---
+++ src/helpers/BuscaNomeBatalhaoReduzidoPorBatalhaoExtendido.ts
@@ -0,0 +1,10 @@
+
+import unidades from '../utils/data/unidades.json'
+
+
+export class BuscaNomeBatalhaoReduzidoPorBatalhaoExtendido{
+  static execute(batalhaoExtendido: string | undefined): string | null {
+    const unidade = unidades.find((unidades) => unidades.UNI_DESCRICAO === batalhaoExtendido)
+    return unidade ? unidade.CD_OPERACIONAL : null
+  }
+}
