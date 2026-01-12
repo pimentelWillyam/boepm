1844bdf2b2d272cdee8f636b2e9bf4edb1801257
Willyam Pimentel <willyampimenteldev@gmail.com>
2026-01-12 15:24:10 -0300

    feat: implementnado busca por bairro no buscador de municipio por nome

--- src/helpers/BuscaMunicipioPorNome.ts
+++ src/helpers/BuscaMunicipioPorNome.ts
@@ -7,13 +7,18 @@ type Municipio = {
 }
 
 import municipiosCompletos from '../pages/Envolvidos/Endereco/municipiosCompletosFinal.json'
+import { BuscaAisPorBairro } from './BuscaAisPorBairro'
 
 export class BuscaMunicipioPorNome {
     private static listaMunicipios: Municipio[] = municipiosCompletos as Municipio[]
-    public static execute(nomeMunicipio: string | undefined): Municipio | null {
+    public static execute(nomeMunicipio: string | undefined, nomeBairro: string | null): Municipio | null {
         console.log("nome", nomeMunicipio)
         const municipioEncontrado = this.listaMunicipios.find(municipio => municipio.MUNICIPIO === nomeMunicipio);
-        if(municipioEncontrado?.MUNICIPIO === 'Recife') console.log('município recifense encontrado!')
+        if(municipioEncontrado?.MUNICIPIO === 'RECIFE') {
+            console.log('município recifense encontrado!')
+            console.log('Buscando AIS por bairro')
+            municipioEncontrado.AIS = BuscaAisPorBairro.execute(nomeBairro) || ''
+        }
         return municipioEncontrado ? municipioEncontrado : null;
     }
 }
